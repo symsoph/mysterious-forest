a66d8f7b11f2a71b42f47660823c767d1b831227
symsoph <fiauyensd@gmail.com>
2023-07-30 23:04:38 -0700

    First commit

---
+++ mysterious forest 2d game w anova 2021/Assets/scrpits/NPC.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NPC : MonoBehaviour
+{
+
+    public GameObject dialogueBox;
+    public TextMeshProUGUI dialogueText;
+    public string NPCText;
+    private bool interactable = false;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            // Player is in talking range of this NPC
+            interactable = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            interactable = false;
+            if (dialogueBox.activeInHierarchy == true)
+            {
+                dialogueBox.SetActive(false);
+            }
+            // Player is no longer in talking range of the NPC 
+        }
+    }
+
+    private void Update()
+    {
+        if (interactable & Input.GetKeyDown(KeyCode.E))
+        {
+            if (dialogueBox.activeInHierarchy == false)
+            {
+                dialogueText.text = NPCText;
+                dialogueBox.SetActive(true);
+            }
+        }
+    }
+}
---
+++ mysterious forest 2d game w anova 2021/Assets/scrpits/PlayerController.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    #region Movement Variables
+
+    public float movementSpeed = 5f; // 5 by default
+    public float jumpForce = 7f;
+    public Rigidbody2D playerRB;
+    public bool grounded;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+
+    #endregion
+
+    #region General Behavior Loop
+
+    private void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+
+    }
+
+    private void Update()
+    {
+        Vector2 movementVector = new Vector2(Input.GetAxis("Horizontal"), 0);
+        TurnAround(movementVector);
+        Move(movementVector);
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            Jump();
+
+            grounded = false;
+        }
+        SmoothenJump();
+    }
+
+
+    // Checking if player returns to the ground and able to jump again
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == ("Ground") && grounded == false)
+        {
+            grounded = true;
+        }
+    }
+
+    #endregion 
+
+    #region Player Movement Functions
+
+    private void Move(Vector2 directionVector)
+    {
+        TurnAround(directionVector);
+        playerRB.velocity = new Vector2(directionVector.x * movementSpeed, playerRB.velocity.y);
+    }
+
+    private void Jump()
+    {
+        playerRB.velocity = new Vector2(playerRB.velocity.x, 0);
+        playerRB.velocity += Vector2.up * jumpForce;
+    }
+
+    private void SmoothenJump()
+    {
+        if (playerRB.velocity.y < 0)
+        {
+            playerRB.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+
+        else if (playerRB.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            playerRB.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+
+        }
+    }
+
+    private void TurnAround(Vector2 directionVector)
+    {
+        if (directionVector == new Vector2(1, 0))
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        else if (directionVector == new Vector2(-1, 0))
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+    #endregion 
+}
